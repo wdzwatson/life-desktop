0597aa4378dad87f78d0cad65e3f9f39fbcc70c4
Shawn_Wang <wang237581102@163.com>
2026-07-21 13:05:32 +0800

    test(tasks): require workspace navigation copy

--- tests/taskNavigationLocales.test.ts
+++ tests/taskNavigationLocales.test.ts
@@ -4,7 +4,21 @@ import { readdirSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 
 const localeDirectory = join(process.cwd(), 'src', 'locales')
-const requiredKeys = ['navigation_label', 'view_modes_label', 'workflow_tools_label']
+const requiredKeys = [
+  'navigation_label',
+  'view_modes_label',
+  'workflow_tools_label',
+  'workspace_label',
+  'overview_label',
+  'stat_open',
+  'stat_today',
+  'stat_overdue',
+  'stat_templates',
+  'instances_group_label',
+  'automation_group_label',
+  'instance_panel_title',
+  'instance_panel_desc',
+]
 const configuredLocales = readdirSync(localeDirectory)
   .filter((filename) => filename.endsWith('.json'))
   .map((filename) => filename.replace(/\.json$/, ''))
